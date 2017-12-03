768a87a43426d7bc15c45322f932eb7ff9c5a16f
Vera Barysava <Vera_Barysava@epam.com>
2017-12-03 19:50:25 +0300

    Final commit

---
+++ babel-cut-logs/index.js
@@ -0,0 +1,28 @@
+
+export default function({ types: t }) {
+    return {
+        name: "babel-cut-logs",
+        visitor: {
+            CallExpression(path, state) {
+                const callee = path.get("callee");
+
+                if (!callee.isMemberExpression()) return;
+
+                if (isConsoleLog(callee)) {
+                    if (path.parentPath.isExpressionStatement()) {
+                        path.remove();
+                    }
+                }
+            }
+        }
+    };
+
+    function isConsoleLog(memberExpr) {
+        const object = memberExpr.get("object");
+        const property = memberExpr.get("property");
+
+        return (
+            object.isIdentifier({ name: "console" }) && property.isIdentifier({ name: "log" })
+        );
+    }
+};
